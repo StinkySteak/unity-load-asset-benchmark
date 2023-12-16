050ba7419d48ee6fc262d8e1488d7fdec7190efe
StinkySteak <slimesteve17@gmail.com>
2023-12-16 22:53:32 +0700

    feat: create dummySO

---
+++ Assets/DummySO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace StinkySteak.Benchmark.LoadAsset
+{
+    [CreateAssetMenu(fileName = nameof(DummySO), menuName = "Data/DummySO")]
+    public class DummySO : ScriptableObject
+    {
+
+    }
+}
